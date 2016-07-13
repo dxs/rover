513ca2e9993a6b753a4d55c1282e66403674836d
glagl <glagl@DESKTOP-47131L7>
2016-07-13 23:38:06 +0200

    Ajoutez des fichiers projet.

---
+++ rover/Landing.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Streams;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace rover
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+		bool test = false;
+        public MainPage()
+		{
+			this.InitializeComponent();
+        }
+
+		private void ClickMe_Click(object sender, RoutedEventArgs e)
+		{
+			send();
+			
+		}
+
+		private constructByte()
+		{
+
+			return;
+		}
+
+		private async void send()
+		{
+			// Get a selector string for bus "I2C1"
+			string aqs = I2cDevice.GetDeviceSelector("I2C1");
+
+			// Find the I2C bus controller with our selector string
+			var dis = await DeviceInformation.FindAllAsync(aqs);
+			if (dis.Count == 0)
+				return; // bus not found
+
+			// 0x40 is the I2C device address
+			var settings = new I2cConnectionSettings(0x07);
+
+			// Create an I2cDevice with our selected bus controller and I2C settings
+			using (I2cDevice device = await I2cDevice.FromIdAsync(dis[0].Id, settings))
+			{
+				byte[] writeBuf = { 0x01, 0x02, 0x03, 0x04 };
+				device.Write(writeBuf);
+			}
+
+		}
+	}
+}
---
+++ rover/Status.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rover
+{
+	class Status
+	{
+		#region var
+		/*Status sendable [27 bytes]*/
+		private byte _StartSend;
+		private byte _PWM;
+		private int _leftSpeed;
+		private byte _leftBrake;
+		private int _rightSpeed;
+		private byte _rightBrake;
+		private int _servo1;
+		private int _servo2;
+		private int _servo3;
+		private int _servo4;
+		private int _servo5;
+		private int _servo6;
+		private byte _devibrate;
+		private int _sensitivity;
+		private int _lowBattery;
+		private byte _I2CAddress;
+		private byte _I2CClock;
+
+		/*Status readable [24 bytes]*/
+		private byte _StartRecv;
+		private byte _errorFlag;
+		private int _batteryVoltage;
+		private int _leftCurrent;
+		private int _leftEncoder;
+		private int _rightCurrent;
+		private int _rightEncoder;
+		private int _Xaxis;
+		private int _Yaxis;
+		private int _Zaxis;
+		private int _deltaX;
+		private int _deltaY;
+		private int _deltaZ;
+
+		#endregion
+
+		#region getSet
+
+		public int leftSpeed { get { return _leftSpeed; } set { _leftSpeed = value; } }
+		public byte leftBrake { get { return _leftBrake; } set { _leftBrake = value; } }
+
+		public int rightSpeed { get { return _rightSpeed; } set { _rightSpeed = value; } }
+		public byte rightBrake { get { return _rightBrake; } set { _rightBrake = value; } }
+
+		public int servo1 { get { return _servo1; } set { _servo1 = value; } }
+		public int servo2 { get { return _servo2; } set { _servo2 = value; } }
+		public int servo3 { get { return _servo3; } set { _servo3 = value; } }
+		public int servo4 { get { return _servo4; } set { _servo4 = value; } }
+		public int servo5 { get { return _servo5; } set { _servo5 = value; } }
+		public int servo6 { get { return _servo6; } set { _servo6 = value; } }
+
+		public byte devibrate   { get { return _devibrate; } set { _devibrate = value; } }
+		public int sensitivity { get { return _sensitivity; } set { _sensitivity = value; } }
+		public int lowBattery  { get { return _lowBattery; } set { _lowBattery = value; } }
+
+		public byte I2CAddress  { get { return _I2CAddress; } set { _I2CAddress = value; } }
+		public byte I2cClock    { get { return _I2CClock; } set { _I2CClock = value; } }
+		#endregion
+
+		public Status()
+		{
+
+		}
+	}
+}

fdd27f3e8222c61584f4c673482b5a103294b5fe
Sven <glaglasven@live.com>
2020-07-01 23:42:08 +0200

    First structure

---
+++ rpi/communications/communications.py
@@ -0,0 +1,6 @@
+## Module communication
+
+class Communications:
+
+    def __init__(self):
+        pass
---
+++ rpi/main.py
@@ -0,0 +1,9 @@
+from controller import Controller
+
+def main():
+    with Controller as control:
+        pass
+
+
+if __name__ == "__main__":
+    main()

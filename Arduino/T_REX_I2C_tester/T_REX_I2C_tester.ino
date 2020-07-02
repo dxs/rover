a8cadef2fdfd2ae2b5145b044cf8cddfe733890d
Sven <glaglasven@live.com>
2020-07-02 14:24:36 +0200

    Update T_REX_I2C_tester.ino

--- Arduino/T_REX_I2C_tester/T_REX_I2C_tester.ino
+++ Arduino/T_REX_I2C_tester/T_REX_I2C_tester.ino
@@ -2,7 +2,7 @@
 
 #define startbyte 0x0F
 #define I2Caddress 0x07
-#define FAILSAFE_TIMEOUT 100 //500ms
+#define FAILSAFE_TIMEOUT 2000 //500ms
 
 int sv[6]={1500,1500,1500,1500,0,0};                 // servo positions: 0 = Not Used
 int sd[6]={5,10,-5,-15,20,-20};                      // servo sweep speed/direction
@@ -24,22 +24,25 @@ int distance = 0;
 
 void setup()
 {  
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.setTimeout(100);
   Wire.begin();                                      // no address - join the bus as master
 }
 
 
 void convert_order(String order)//format LEFT[0-255],RIGHT[0-255];
 {
-  if(order.indexOf("SEN") >= 0)
-  {
-    MasterReceive();                                   // receive data packet from T'REX controller
-    return;
-  }
-
+  
   if(order.indexOf("DIS") >= 0)
   {
     Serial.println(distance);
+    return;
+  }
+
+  
+  if(order.indexOf("SEN") >= 0)
+  {
+    MasterReceive();                                   // receive data packet from T'REX controller
     return;
   }
   
@@ -52,22 +55,22 @@ void convert_order(String order)//format LEFT[0-255],RIGHT[0-255];
   token = strtok(NULL, delim);
   rmspeed = atoi(token);
   Serial.println("OK");
+  MasterSend(startbyte,2,lmspeed,lmbrake,rmspeed,rmbrake,sv[0],sv[1],sv[2],sv[3],sv[4],sv[5],devibrate,sensitivity,lowbat,i2caddr,i2cfreq);
 }
 
 void loop()
 {
-                                                     // send data packet to T'REX controller 
-  MasterSend(startbyte,2,lmspeed,lmbrake,rmspeed,rmbrake,sv[0],sv[1],sv[2],sv[3],sv[4],sv[5],devibrate,sensitivity,lowbat,i2caddr,i2cfreq);
 
   //Get distance
   distance = analogRead(distance_pin);
-  delay(50);
-  failsafe++;
-  
+  delay(5);
+
   if(failsafe >= FAILSAFE_TIMEOUT || distance > 200) //approx 30cm distance
   {
     lmspeed = 0;
     rmspeed = 0;
+    failsafe = 0;
+    MasterSend(startbyte,2,lmspeed,lmbrake,rmspeed,rmbrake,sv[0],sv[1],sv[2],sv[3],sv[4],sv[5],devibrate,sensitivity,lowbat,i2caddr,i2cfreq);
   }
   while (Serial.available() > 0) {
     failsafe = 0;
@@ -76,4 +79,6 @@ void loop()
     incoming = Serial.readString();
     convert_order(incoming);
   }
+
+  failsafe++;
 }
